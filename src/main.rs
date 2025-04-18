498fc71fe8c58cc41897641881dcc25f4d124010
Chalk <61552486+zwsyscall@users.noreply.github.com>
2025-04-18 22:31:15 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod evasion;
+
+fn main() {
+    // Decrypt the package
+    // Ensure it matches some checksum
+    // Create a child with mapviewoffile and inject into it :^)
+    // Now, we have to somehow avoid antivirus software while doing this
+}
